195f8942a3ebd18f89a846dd76520fad29dcd0f2
Matt Woenker <matt@clickhouse.com>
2026-01-16 11:34:00 -0500

    Remove console.log

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { startAnimation } from "./controller.js";
-import { die } from "./die.js";
 
 window.addEventListener("load", () => {
   const canvas = document.getElementById("screen") as HTMLCanvasElement;
   startAnimation(canvas);
   const resize = () => {
-    console.log("do");
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    console.log("did");
   };
   window.addEventListener("resize", resize);
   resize();
